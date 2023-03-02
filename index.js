783de1abf845bbc1acfd3bac247530843a41f8a4
Fahad <kfahad5607@gmail.com>
2023-03-02 14:17:45 +0530

    Print in single page

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+
+const docHeight = () => {
+  const body = document.body
+  const html = document.documentElement;
+
+  let height =  Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+  height = parseInt(height * 0.9) ;
+
+  return height + 'px';
+}
 
 (async () => {
-
   // Create a browser instance
   const browser = await puppeteer.launch();
 
@@ -9,21 +18,32 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
 
   // Website URL to export as pdf
-  const website_url = 'http://127.0.0.1:5500/index.html'; 
+  const website_url = "http://127.0.0.1:5500/index.html";
 
   // Open URL in current page
-  await page.goto(website_url, { waitUntil: 'networkidle0' }); 
+  await page.goto(website_url, { waitUntil: "networkidle0" });
 
   //To reflect CSS used for screens instead of print
-  await page.emulateMediaType('screen');
+  await page.emulateMediaType("screen");
 
-// Downlaod the PDF
+  // const height_weight_ratio = await page.evaluate( () => window.innerHeight / window.innerWidth)
+  // let height = parseInt('10.8cm') * height_weight_ratio
+  // const scale = 1/height_weight_ratio;
+  let height = await page.evaluate(docHeight);
+  
+  // height += 'px'
+  // console.log("height ", height);
+
+
+  // Downlaod the PDF
   const pdf = await page.pdf({
-    path: 'static/docs/result.pdf',
-    margin: { top: '0px', right: '10px', bottom: '0px', left: '10px' },
+    path: "static/docs/resume.pdf",
+    margin: { top: "0px", right: "10px", bottom: "0px", left: "10px" },
     printBackground: true,
-    format: 'A4',
+    // format: "A4",
+    height: height,
     scale: 0.9,
+    // preferCSSPageSize: false,
   });
 
   // Close the browser instance
@@ -32,3 +52,5 @@ const puppeteer = require('puppeteer');
 
 // visit this website and merge the pages
 // https://avepdf.com/convert-to-one-page-pdf
+
+
